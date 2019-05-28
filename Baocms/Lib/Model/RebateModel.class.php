097cf9768ce64e3b2564a57230c458de811e30c7
lvliukun <791419612@qq.com>
2019-05-28 15:47:18 +0800

    zhifu

--- Baocms/Lib/Model/RebateModel.class.php
+++ Baocms/Lib/Model/RebateModel.class.php
@@ -7,10 +7,10 @@
  */
 class RebateModel extends Model {
 
-    public function fy($list,$use_id,$rate,$erweima_id,$business_code,$out_uid){
+    public function fy($order_id,$list,$use_id,$rate,$erweima_id,$business_code,$out_uid){
         $money = $list * $rate;
         if($money<0 || $money==0 ){return;}
-        $this->rebate($use_id,$money,$erweima_id,$business_code,$out_uid);
+        $this->rebate($order_id,$use_id,$money,$erweima_id,$business_code,$out_uid);
         $userinfo = D('Users')->where(array('pid'=>$use_id))->field('user_id,rate')->find();
         if($userinfo['user_id']){
             $puse_id =$userinfo['user_id'];
@@ -18,22 +18,23 @@ class RebateModel extends Model {
             $rate = $rate * 10000;
             $pmoney = ($prate - $rate) * $list / 10000;
             if($pmoney<0 || $pmoney==0 ){return;}
-            $this->rebate($puse_id,$pmoney,$erweima_id,$business_code,$out_uid);
+            $this->rebate($order_id,$puse_id,$pmoney,$erweima_id,$business_code,$out_uid);
             $userinfo1 = D('Users')->where(array('pid'=>$puse_id))->field('user_id,rate')->find();
             if($userinfo1['user_id']){
                 $duse_id =$userinfo1['user_id'];
                 $drate =$userinfo['rate'] * 10000;
                 $dmoney = ($drate - $prate) * $list /10000;
                 if($dmoney<0 || $dmoney==0){return;}
-                $this->rebate($duse_id,$dmoney,$erweima_id,$business_code,$out_uid);
+                $this->rebate($order_id,$duse_id,$dmoney,$erweima_id,$business_code,$out_uid);
             }
 
         }
     }
 
-    private function rebate($user_id,$money,$erweima_id,$business_code,$out_uid){
+    private function rebate($order_id,$user_id,$money,$erweima_id,$business_code,$out_uid){
         $data=array(
             'user_id'=>$user_id,
+            'order_id'=>$order_id,
             'score'=>$money,
             'erweima_id'=>$erweima_id,
             'business_code'=>$business_code,
