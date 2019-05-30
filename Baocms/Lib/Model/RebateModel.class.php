f413433af02474c049787bfee504aaf35d9b2f79
lvliukun <791419612@qq.com>
2019-05-30 10:59:44 +0800

    zhifu

--- Baocms/Lib/Model/RebateModel.class.php
+++ Baocms/Lib/Model/RebateModel.class.php
@@ -7,11 +7,11 @@
  */
 class RebateModel extends Model {
 
-    public function fy($order_id,$list,$use_id,$rate,$erweima_id,$business_code,$out_uid){
+    public function fy($order_id,$list,$use_id,$rate,$pid,$erweima_id,$business_code,$out_uid){
         $money = $list * $rate;
         if($money<0 || $money==0 ){return;}
         $this->rebate($order_id,$use_id,$money,$erweima_id,$business_code,$out_uid);
-        $userinfo = D('Users')->where(array('pid'=>$use_id))->field('user_id,rate')->find();
+        $userinfo = D('Users')->where(array('user_id'=>$pid))->field('user_id,pid,rate')->find();
         if($userinfo['user_id']){
             $puse_id =$userinfo['user_id'];
             $prate =$userinfo['rate'] * 10000;
@@ -19,10 +19,10 @@ class RebateModel extends Model {
             $pmoney = ($prate - $rate) * $list / 10000;
             if($pmoney<0 || $pmoney==0 ){return;}
             $this->rebate($order_id,$puse_id,$pmoney,$erweima_id,$business_code,$out_uid);
-            $userinfo1 = D('Users')->where(array('pid'=>$puse_id))->field('user_id,rate')->find();
+            $userinfo1 = D('Users')->where(array('user_id'=>$userinfo['pid']))->field('user_id,rate')->find();
             if($userinfo1['user_id']){
                 $duse_id =$userinfo1['user_id'];
-                $drate =$userinfo['rate'] * 10000;
+                $drate =$userinfo1['rate'] * 10000;
                 $dmoney = ($drate - $prate) * $list /10000;
                 if($dmoney<0 || $dmoney==0){return;}
                 $this->rebate($order_id,$duse_id,$dmoney,$erweima_id,$business_code,$out_uid);
